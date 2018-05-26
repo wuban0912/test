d149f57ff14bb6f30820dda9cd8e6fa0f04c1b57
wuban <Administrator@SC-201803242046>
2018-05-27 00:05:38 +0800

    Revert "tetw"
    This reverts commit 4e4be2caf45fc156f6a62ceb299b36665ba3c7cc.

--- src/test/test.java
+++ src/test/test.java
@@ -15,7 +15,49 @@ import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConv
 public class test {
 
 	public static void main(String[] args) throws Exception {
-		 
+		// 是否可预览标志 0普通文档，MP3，SWF，可以预览 ， 1 不支持的文件类型，不能预览 2，MP4播放
+		String previewFlag = "0";
+		String pathString = "C:\\Users\\Administrator\\Downloads\\";
+		String newFileName = "采购订单EXT002201810014.xlsx";
+		String startupPath = "C:\\Program Files (x86)\\";
+		Runtime rt = Runtime.getRuntime();
+		rt.exec(startupPath+"OpenOffice 4\\program\\soffice.exe -headless -nologo -norestore -accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager");
+		/************************************** 以下为文件预览 *****************************************************/
+		if (previewFlag.equals("0")) {
+			// 服务器信息
+			Properties props = System.getProperties();
+			// 不同系统分隔符
+			String separator = props.getProperty("file.separator");
+			// 服务器存储的文件名称 201212171709212889.doc
+			String sysFileName = pathString + newFileName;
+			// linux 下
+			// 是一个正斜杠/home/apache-tomcat-6.0.33/webapps/jd/WebRoot/upload/knowledge/201212181318342606.ppt
+			String fullPath = pathString + newFileName;
+			// 与sysFileName保持一致,如果已经存在，不再每次生成
+			String sysFileName_temp = sysFileName.substring(0, sysFileName.lastIndexOf("."));
+
+			File sourceFile; // 转换源文件
+			File pdfFile; // PDF媒介文件
+			// 非PDF格式文件处理方式
+			sourceFile = new File(fullPath);
+			pdfFile = new File(separator + sysFileName_temp + ".pdf");
+			rt = Runtime.getRuntime();
+			if (!pdfFile.exists()) {
+				// 获取连接对象
+				OpenOfficeConnection connection = new SocketOpenOfficeConnection(8100);
+				// 取得连接
+				connection.connect();
+				// 创建文件格式转换对象
+				DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
+				// 实现文件格式转换
+				converter.convert(sourceFile, pdfFile);
+				// 生成已转换的PDF文件
+				pdfFile.createNewFile();
+				// 释放连接
+				connection.disconnect();
+			}
+		}
+
 	}
 
 	/**
